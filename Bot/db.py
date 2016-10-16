c981401aa4f00c351b3b75e08eb2d69a816fb459
clément landier <clment landier>
2016-10-16 15:31:06 +0200

    TEST

--- Bot/db.py
+++ Bot/db.py
@@ -12,12 +12,21 @@ class Playlist():
 		)
 		""")
 		self.db.commit()
-	def addsong(self, lien):
+
+
+	def addSong(self, lien):
+		"""
+		Ajoute un lien dans la BDD (return : True)
+		"""
 		self.cursor.execute("""INSERT INTO playlist(lien) VALUES(?)""", [lien])
 		self.db.commit()
+		return True
 
 
-	def getall(self):
+	def getAllLien(self):
+		"""
+		Recupere tous les liens dans la BDD (return : list)
+		"""
 		self.reponse = []
 		self.cursor.execute("""SELECT lien FROM playlist""")
 		rows = self.cursor.fetchall()
@@ -26,4 +35,58 @@ class Playlist():
 		return self.reponse
 
 
+	def getAllId(self):
+		"""
+		Recupere tous les id dans la BDD (return : list)
+		"""
+		self.reponsess = []
+		self.cursor.execute("""SELECT id FROM playlist""")
+		rows = self.cursor.fetchall()
+		for row in rows:
+			self.reponsess.append('{0}'.format(row[0]))
+		self.reponsess = [int(x) for x in self.reponsess]
+		return self.reponsess
 
+
+	def getAllDic(self,debug = False):
+		"""
+		Recupere tous les liens et id dans la BDD (return : dict)
+		"""
+		self.reponses = {}
+		lien = self.getAllLien()
+		id = self.getAllId()
+		for i in range(len(id)):
+			self.reponses[id[i]]=lien[i]
+		return self.reponses
+
+
+	def delSongLien(self, lien):
+		"""
+		Supprime un lien (par le lien) dans la BDD (return : True)
+		"""
+		self.cursor.execute("""DELETE FROM playlist WHERE lien=?""", [lien])
+		self.db.commit()
+
+
+	def delSongId(self, id):
+		"""
+		Supprime un lien (par l id) dans la BDD (return : True)
+		"""
+		self.cursor.execute("""DELETE FROM playlist WHERE id=?""", [id])
+		self.db.commit()
+
+
+	def changeSongLienByLien(self,originallien, newlien):
+		"""
+		Change un lien (par le lien) dans la BDD (return : True)
+		"""
+		self.cursor.execute("""UPDATE playlist SET lien = ? WHERE lien = ?""", (newlien,originallien))
+		self.db.commit()
+
+		
+	def changeSongLienById(self,id , newlien):
+		"""
+		Change un lien (par l id) dans la BDD (return : True)
+		"""
+		self.cursor.execute("""UPDATE playlist SET lien = ? WHERE id = ?""", (newlien,id))
+		self.db.commit()
